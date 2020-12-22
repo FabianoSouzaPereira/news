1d258b636c7def1f552eeb4d6331141724b7ab4b
Fabiano Pereira <jeovajire2003@gmail.com>
2020-12-22 11:53:37 -0300

    add routes

---
+++ src/routes/index.tsx
@@ -0,0 +1,24 @@
+import React, { useContext } from 'react';
+import { View, ActivityIndicator } from 'react-native';
+
+import AuthContext from '../contexts/auth';
+
+import AuthRoutes from '../routes/auth.routes';
+import AppRoutes from '../routes/app.routes';
+
+const Routes: React.FC = () => {
+  const { signed, loading } = useContext(AuthContext);
+
+  if (loading) {
+    return (
+      // eslint-disable-next-line react-native/no-inline-styles
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <ActivityIndicator size="large" color="#666" />
+      </View>
+    );
+  }
+
+  return signed ? <AppRoutes /> : <AuthRoutes />;
+};
+
+export default Routes;
